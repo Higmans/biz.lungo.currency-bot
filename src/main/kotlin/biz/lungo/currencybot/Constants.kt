f9d3ec3a40e630e0a5cd89c5e68a385afbd8f686
Elijah <i@lungo.biz>
2022-10-26 17:56:00 +0300

    Ukrainian jokes!

--- src/main/kotlin/biz/lungo/currencybot/Constants.kt
+++ src/main/kotlin/biz/lungo/currencybot/Constants.kt
@@ -2,6 +2,6 @@ package biz.lungo.currencybot
 
 const val BOT_API_URL = "https://api.telegram.org"
 const val NBU_RATES_PATH = "https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json"
-const val RANDOM_JOKE_URL = "https://randstuff.ru/joke/"
+const val RANDOM_JOKE_URL = "https://rozdil.lviv.ua/anekdot/anekdot.php?id="
 const val CRYPTO_RATES_BASE_URL = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?symbol="
 const val OIL_URL = "https://deeptest.lungo.biz/oil.html"
--- src/main/kotlin/biz/lungo/currencybot/JokeScraper.kt
+++ src/main/kotlin/biz/lungo/currencybot/JokeScraper.kt
@@ -5,26 +5,21 @@ import it.skrape.fetcher.HttpFetcher
 import it.skrape.fetcher.response
 import it.skrape.fetcher.skrape
 import it.skrape.selects.html5.*
+import kotlin.random.Random
 
 suspend fun getNewJoke(): String {
     var text = ""
 
     skrape(HttpFetcher) {
-        request { url = RANDOM_JOKE_URL }
+        request { url = RANDOM_JOKE_URL + Random.nextInt(1, 9247) }
         response {
             htmlDocument {
-                body {
-                    table {
-                        withClass = "text"
-                        text = findFirst {
-                            tbody {
-                                tr {
-                                    td {
-                                        findFirst {
-                                            this.text
-                                        }
-                                    }
-                                }
+                p {
+                    withClass = "coupon-field"
+                    text = findFirst {
+                        i {
+                            findFirst {
+                                this.text
                             }
                         }
                     }
